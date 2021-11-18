26e20db40f0a82222e0d34f7c211ffe6e46e36de
sartajmahmud <oishik1999@gmail.com>
2021-11-18 16:35:00 +0600

    OrderDetailsScreen Completed

--- lib/screens/OrderDetailsScreen.dart
+++ lib/screens/OrderDetailsScreen.dart
@@ -1,9 +1,8 @@
-
 import 'package:flutter/material.dart';
 import 'package:grocery_app/common_widgets/app_text.dart';
 
 class OrderDetailsScreen extends StatefulWidget {
-   OrderDetailsScreen();
+  OrderDetailsScreen();
 
   @override
   _OrderDetailsScreenState createState() => _OrderDetailsScreenState();
@@ -42,22 +41,243 @@ class _OrderDetailsScreenState extends State<OrderDetailsScreen> {
         ),
       ),
       body: Container(
+        padding: EdgeInsets.all(15),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Order Details'),
-            Text('Order Number : 1221413780'),
-            Text('Order from : XYZ Company'),
+            AppText(
+              text: 'Order Details',
+              fontSize: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  AppText(
+                    text: 'Your order number:',
+                    fontSize: 16,
+                  ),
+                  Text(
+                    '1234',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: Colors.lightBlue,
+                        fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  AppText(
+                    text: 'Ordered from:',
+                    fontSize: 16,
+                  ),
+                  Text(
+                    'XYZ Company',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: Colors.red,
+                        fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  AppText(
+                    text: 'Seller Mobile No:',
+                    fontSize: 16,
+                  ),
+                  Text(
+                    '01794613464',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black,
+                        fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  AppText(
+                    text: 'Pickup location:',
+                    fontSize: 16,
+                  ),
+                  Text(
+                    'Dhanmondi, Dhaka',
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black,
+                        fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+
             ///Need to add a button for navigating to Maps location screen
-            Text('PickUp Location'),
-            Divider(thickness: 2,),
+            Divider(
+              thickness: 2,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                      flex: 1,
+                      child: AppText(
+                        text: '2x',
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      )),
+                  Expanded(
+                      flex: 6,
+                      child: AppText(
+                        text: 'Bizol Ultra Engine OIL 2L',
+                        fontSize: 15,
+                      )),
+                  Expanded(
+                      flex: 2,
+                      child: AppText(
+                          text: '\৳12.96',
+                          fontSize: 15,
+                          textAlign: TextAlign.end)),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                      flex: 1,
+                      child: AppText(
+                        text: '2x',
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      )),
+                  Expanded(
+                      flex: 6,
+                      child: AppText(
+                        text: 'Bizol Ultra Engine OIL 2L',
+                        fontSize: 15,
+                      )),
+                  Expanded(
+                      flex: 2,
+                      child: AppText(
+                          text: '\৳12.96',
+                          fontSize: 15,
+                          textAlign: TextAlign.end)),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                      flex: 1,
+                      child: AppText(
+                        text: '2x',
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      )),
+                  Expanded(
+                      flex: 6,
+                      child: AppText(
+                        text: 'Bizol Ultra Engine OIL 2L',
+                        fontSize: 15,
+                      )),
+                  Expanded(
+                      flex: 2,
+                      child: AppText(
+                        text: '\৳12.96',
+                        fontSize: 15,
+                        textAlign: TextAlign.end,
+                      )),
+                ],
+              ),
+            ),
+
             ///listview of the order items.
-            Text("Bizol Moto Engine oil 4L x1       300TK"),
-            Text("Bizol Moto Engine oil 1L x3       400TK "),
-            Text("Bizol Car Engine oil 4L x2        500TK"),
-            Divider(thickness: 2,),
-            Text('SubTotal                              1200TK'),
-            Text('VAT 15%                             180TK'),
-            Text('Total (Including VAT)                      1380TK'),
+            Divider(
+              thickness: 2,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  AppText(
+                    text: 'Subtotal:',
+                    fontSize: 16,
+                  ),
+                  Text(
+                    '\৳12.96',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black,
+                        fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  AppText(
+                    text: 'VAT (15%) :',
+                    fontSize: 16,
+                  ),
+                  Text(
+                    '\৳12.96',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black,
+                        fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+            Divider(
+              thickness: 1,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  AppText(
+                    text: 'Total:',
+                    fontSize: 16,
+                  ),
+                  Text(
+                    '\৳12.96',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black,
+                        fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
